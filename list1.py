66db4401dc7d4f35746d8261a89247136ce98a64
Debajyoti Majumder <debajyoti344@gmail.com>
2025-05-03 19:30:20 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,8 @@
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+list=[a,b,c]
+print(list)
+d=int(input("enter the value of d"))
+list.append(d)
+print(list)
