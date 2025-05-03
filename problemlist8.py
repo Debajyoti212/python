3534484d987f4a2d752e4e475b973f164a88b2fd
Debajyoti Majumder <debajyoti344@gmail.com>
2025-05-03 22:05:04 +0530

    Add files via upload

---
+++ problemlist8.py
@@ -0,0 +1,8 @@
+##wap tov ask the user to enter names of 3 movies and store them in a list
+movies=[]
+firstmovie=input("enter the movie ")
+secondmovie=input("enter the movie ")
+thirdmovie=input("enter the movie ")
+d=firstmovie,secondmovie,thirdmovie
+movies.append(d)
+print(movies)
